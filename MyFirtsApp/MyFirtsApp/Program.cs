7a0ed2c34d6145cfb82489ea3db5b1704667a972
s27928 <s27928@pjwstk.edu.pl>
2024-03-12 17:00:24 +0100

    GetAverage Method

--- MyFirtsApp/MyFirtsApp/Program.cs
+++ MyFirtsApp/MyFirtsApp/Program.cs
@@ -5,4 +5,16 @@ Console.WriteLine("Modification1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
 
+static double GetAverage(int [] numbers)
+{
+    double counter = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        
+        counter += numbers[i];
+        
+    }
 
+    return counter / numbers.Length;
+
+}
