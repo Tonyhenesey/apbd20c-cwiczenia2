b6de806fae5cb6aa3be855eabbaa0c0e88a614f8
s27928 <s27928@pjwstk.edu.pl>
2024-03-13 20:19:27 +0100

    Txt change Modification3->modification 5

--- MyFirtsApp/MyFirtsApp/Program.cs
+++ MyFirtsApp/MyFirtsApp/Program.cs
@@ -3,6 +3,6 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification1");
 Console.WriteLine("Modification 2");
-Console.WriteLine("Modification 3");
+Console.WriteLine("Modification 5");
 
 
