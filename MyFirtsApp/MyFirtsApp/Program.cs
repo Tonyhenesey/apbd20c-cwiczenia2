9b62835f7546a256339f2c4f2c5fa5882846f8f5
s27928 <s27928@pjwstk.edu.pl>
2024-03-13 11:07:32 +0100

    Update Program.cs
    modyfikacja testowa

--- MyFirtsApp/MyFirtsApp/Program.cs
+++ MyFirtsApp/MyFirtsApp/Program.cs
@@ -22,3 +22,5 @@ static double GetAverage(int [] numbers)
 int[] numbers = {1,2,3,4};
 double average = GetAverage(numbers);
 Console.WriteLine(average);
+
+Console.WriteLine("Modyfikacja testowa");
